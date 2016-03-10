4c78f9de73a5768c3d112d989f486734f203edd1
Ian Griffiths <ian@interact-sw.co.uk>
2016-03-10 07:28:45 +0000

    Add task timeout extension methods.
    When testing async code, it's often useful to wait for something to
    happen but being prepared for it not to happen because of a fault in
    the code under test (or because you've not written the code required to
    pass the test yet). So it can be helpful to wait for a reasonable time
    and then give up with an error.
    
    These new WithTimeout extension methods for Task make this easy.

--- src/Idg.AsyncTestTools/TaskExtensions/AsyncTestTaskExtensions.cs
+++ src/Idg.AsyncTestTools/TaskExtensions/AsyncTestTaskExtensions.cs
@@ -32,5 +32,38 @@ namespace Idg.AsyncTest.TaskExtensions
                 },
                 TaskContinuationOptions.ExecuteSynchronously);
         }
+
+        /// <summary>
+        /// Produces a task that completes when the input task completes, unless the specified
+        /// timeout elapses, in which case it faults with a <see cref="TimeoutException"/>.
+        /// </summary>
+        /// <param name="t">The task to wait for.</param>
+        /// <param name="timeout">The maximum time to wait before timing out.</param>
+        /// <returns>
+        /// A task that represents the outcome of the input task, unless the input task did not
+        /// complete within the specified time, in which case the returned task faults with
+        /// <see cref="TimeoutException"/>.
+        /// </returns>
+        public static async Task WithTimeout(this Task t, TimeSpan timeout)
+        {
+            await Task.WhenAny(t, Task.Delay(timeout));
+            if (!t.IsCompleted)
+            {
+                throw new TimeoutException();
+            }
+            await t;
+        }
+
+        /// <summary>
+        /// Produces a task that completes when the input task completes, unless two seconds
+        /// pass, in which case it faults with a <see cref="TimeoutException"/>.
+        /// </summary>
+        /// <param name="t">The task to wait for.</param>
+        /// <returns>
+        /// A task that represents the outcome of the input task, unless the input task did not
+        /// complete within the specified time, in which case the returned task faults with
+        /// <see cref="TimeoutException"/>.
+        /// </returns>
+        public static Task WithTimeout(this Task t) => t.WithTimeout(TimeSpan.FromSeconds(2));
     }
 }
